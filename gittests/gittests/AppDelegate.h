0132925bcd4d411586335f4d86c5c451888a22ef
775842767 <13071159748@yicai.com>
2016-06-17 12:20:07 +0800

    sdf
    dsfsd

---
+++ gittests/gittests/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  gittests
+//
+//  Created by 冯勇 on 16/6/17.
+//  Copyright © 2016年 冯勇. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ gittests/gittests/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  gittests
+//
+//  Created by 冯勇 on 16/6/17.
+//  Copyright © 2016年 冯勇. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    //122345566
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
